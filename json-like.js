49d8a4a24554703b9deaddb09d8061568ddec0f0
Aria Stewart <aredridel@dinhe.net>
2015-02-20 22:00:48 -0800

    Extend json-like grammar

--- json-like.js
+++ json-like.js
@@ -11,9 +11,26 @@ var grammar = Grammar([
     Rule('value', [ Ref('string') ]),
     Rule('value', [ Ref('number') ]),
     Rule('value', [ Ref('boolean') ]),
+    Rule('number', [ Ref('digit') ]),
+    Rule('number', [ Ref('number'), Ref('digit') ]),
+    Rule('digit', [ Terminal('0') ]),
+    Rule('digit', [ Terminal('1') ]),
+    Rule('digit', [ Terminal('2') ]),
+    Rule('digit', [ Terminal('3') ]),
+    Rule('digit', [ Terminal('4') ]),
+    Rule('digit', [ Terminal('5') ]),
+    Rule('digit', [ Terminal('6') ]),
+    Rule('digit', [ Terminal('7') ]),
+    Rule('digit', [ Terminal('8') ]),
+    Rule('digit', [ Terminal('9') ]),
     Rule('object', [ Terminal('{'), Ref('pairs'), Terminal('}') ]),
-    Rule('pairs', [ Ref('string'), Terminal(':'), Ref('value') ]),
-    Rule('string', [ Terminal('"'), Terminal('a'), Terminal('"') ])
+    Rule('pairs', [ Ref('pair') ]),
+    Rule('pairs', [ Ref('pair'), Terminal(','), Ref('pairs') ]),
+    Rule('pair', [ Ref('string'), Terminal(':'), Ref('value') ]),
+    Rule('string', [ Terminal('"'), Ref('inside-string'), Terminal('"') ]),
+    Rule('string', [ Terminal('"'), Terminal('"') ]),
+    Rule('inside-string', [ Ref('inside-string'), Terminal('a') ]),
+    Rule('inside-string', [ Terminal('a') ]),
 ]);
 
-parse(grammar, '{"a":"a"}');
+parse(grammar, '{"a":"aaaaaaaaa","a":0123}');
