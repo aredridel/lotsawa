7b47fed88abfc6cbfa6997edb87c3b1d88dee3a4
Aria Stewart <aredridel@nbtsc.org>
2015-02-22 19:26:13 -0500

    Make the exercise script tidier

--- exercise.js
+++ exercise.js
@@ -6,12 +6,10 @@ var Terminal = require('./').Terminal;
 var debug = require('./debug');
 
 var grammar = Grammar([
-  Rule('start', []),
-  Rule('start', [Terminal('a'), Ref('start')])
+  Rule('start', [ Terminal('b') ]),
+  Rule('start', [ Terminal('a'), Ref('start') ])
 ]);
 
 console.log(debug.dump_grammar(grammar));
 
-var success = debug.parse(grammar, 'aa');
-
-console.log('parse', success ? 'succeeded' : 'failed');
+debug.parse(grammar, 'aab');
