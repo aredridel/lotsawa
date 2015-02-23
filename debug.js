2ea6e2cd1d9e6c53320baa95a52cb9419b979c92
Aria Stewart <aredridel@nbtsc.org>
2015-02-22 23:21:25 -0500

    fix debug display of rules

--- debug.js
+++ debug.js
@@ -2,11 +2,11 @@ var chalk = require('chalk');
 var bitmv = require('bitmv');
 var parse = require('./').parse;
 
-function dump_rule(rule) {
+function dump_rule(grammar, rule) {
   return chalk.white('{') + chalk.yellow(rule.name) + chalk.white(' → ') + chalk.cyan(rule.symbols.map(display).join(' ')) + chalk.white('}');
 
   function display(e) {
-    return e.name;
+    return grammar.symbols[e];
   }
 }
 
@@ -29,7 +29,7 @@ function dump_grammar(grammar) {
   var out = '';
   log('rules');
   grammar.forEach(function(e, i) {
-    log(i + ' ' + dump_rule(e));
+    log(i + ' ' + dump_rule(grammar, e));
   });
 
   log('symbols');
