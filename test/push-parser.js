3c4ba27c0a2cc27d8aecce5cc819a7c259462931
Aria Stewart <aredridel@nbtsc.org>
2015-03-18 00:42:47 -0400

    Add push parser test

---
+++ test/push-parser.js
@@ -0,0 +1,22 @@
+var Grammar = require('../').Grammar;
+var Rule = require('../').Rule;
+var Ref = require('../').Ref;
+var Parser = require('../').Parser;
+var Terminal = require('../').Terminal;
+var test = require('tape');
+
+var grammar = Grammar([
+    Rule('start', [ Ref('A') ]),
+    Rule('A', [ Ref('A'), Terminal('a') ]),
+    Rule('A', [ Terminal('a') ])
+]);
+
+test('parses using externally driven parser', function (t) {
+    var p = Parser(grammar);
+    var str = 'aaaaaaaaaaa';
+    for (var i = 0; i < str.length; i++) {
+        p.push(str[i]);
+    }
+    t.ok(p.success());
+    t.end();
+});
